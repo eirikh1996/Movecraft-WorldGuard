301d4f95a5ef41cbbfa3891c68677e6807afc82f
TylerS1066 <TylerS1066@users.noreply.github.com>
2021-03-06 19:36:13 -0600

    We have something

---
+++ src/main/java/net/countercraft/movecraft/worldguard/MovecraftWorldGuard.java
@@ -0,0 +1,17 @@
+package net.countercraft.movecraft.worldguard;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class MovecraftWorldGuard extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
